62242c2aeacd6e7e2e01c6acf1311380d2d3dd5a
rishabkhoslaIslabs <40018474+rishabkhoslaIslabs@users.noreply.github.com>
2018-06-10 00:39:14 +0530

    Add files via upload

---
+++ rishabh.khosla/core-java/Modifiers/src/com/access/modifiers/Execution.java
@@ -0,0 +1,12 @@
+package com.access.modifiers;
+
+public class Execution {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Private rr=new Private();
+		rr.print();
+
+	}
+
+}
---
+++ rishabh.khosla/core-java/Modifiers/src/com/access/modifiers/Private.java
@@ -0,0 +1,14 @@
+package com.access.modifiers;
+
+public class Private {
+	private int number=10;
+	
+	
+	
+	private void print(){
+		
+		System.out.println("this a private method");
+	}
+	
+
+}
